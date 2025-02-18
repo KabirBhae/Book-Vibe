a46b81907af78efa6b8591483e682e94940f0b34
Arafat Kabir Khan <arafatkabirkhan22@gmail.com>
2025-02-18 06:21:35 +0600

    minor ui tweaks

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,14 @@ const Banner = () => {
 				<div className="px-6">
 					<h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
 					<p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-					<a href="#book-catalog">
+					<a href="#book-catalog"
+						onClick={e => {
+							e.preventDefault();
+							document.querySelector("#book-catalog").scrollIntoView({
+								behavior: "smooth",
+							});
+						}}
+					>
 						<button className="btn btn-primary">Get Started</button>
 					</a>
 				</div>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -9,8 +9,8 @@ const Books = () => {
 			.then(data => setBooks(data));
 	}, []);
 	return (
-		<div>
-			<h2 id="book-catalog" className="text-4xl font-bold text-center mt-25 mb-9">Books</h2>
+		<div id="book-catalog">
+			<h2 className="text-4xl font-bold text-center mt-25 mb-9">Books</h2>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 				{books.map(bookItem => (
 					<Book key={bookItem.bookId} book={bookItem}></Book>
